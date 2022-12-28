4fbcb2c44884229adffdbe8ff778fffc2e5f7159
motty93 <rdwbocungelt5@gmail.com>
2022-12-28 23:08:11 +0900

    feat: case

---
+++ elixir-lang/case.exs
@@ -0,0 +1,29 @@
+case {1, 2, 3} do
+  {1, x, 3} when x > 0 ->
+    IO.puts("Will match")
+
+  _ ->
+    IO.puts("Would match, if guard condition were not satisfied")
+end
+
+x = 1
+case 10 do
+  ^x ->
+    IO.puts("Won't match")
+
+  _ ->
+    IO.puts("Will match")
+end
+
+f = fn
+  x, y when x > 0 -> x + y
+  x, y -> x * y
+end
+
+IO.puts(f.(1, 3))
+
+# 無名関数の中で節は引数の数が同じでないといけない
+# f2 = fn
+#   x, y when x > 0 -> x + y
+#   x, y, z -> x * y + z
+# end
