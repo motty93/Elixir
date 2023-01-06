2c0b364ebda9c2bbbbfcd7f0f6b9ae0072a9dc97
motty93 <rdwbocungelt5@gmail.com>
2023-01-06 22:50:38 +0900

    feat: noname function &記法

--- elixir-lang/noname_function.exs
+++ elixir-lang/noname_function.exs
@@ -1,6 +1,8 @@
 add = fn a, b -> a + b end
+add2 = &(&1 + &2)
 
 IO.puts(add.(1, 2))
+IO.inspect(add2.(1, 2), label: "＆省略記法")
 
 IO.puts(is_function(add))
 
@@ -8,4 +10,6 @@ IO.puts(is_function(add))
 IO.puts(is_function(add, 2))
 
 double = fn a -> add.(a, a) end
+double2 = &add2.(&1, &1)
 IO.puts(double.(2))
+IO.inspect(double2.(2), label: "＆省略記法")
