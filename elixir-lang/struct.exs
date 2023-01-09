0a19e96cf3d39b548d3f06e78ca353d87a79cea0
motty93 <rdwbocungelt5@gmail.com>
2023-01-09 20:33:50 +0900

    feat: struct @derive

--- elixir-lang/struct.exs
+++ elixir-lang/struct.exs
@@ -1,10 +1,13 @@
 defmodule User do
+  @derive {Inspect, only: [:name]}
+  # @derive {Inspect, except: [:age]}と同じ
   defstruct name: "motty", age: 31
 end
 
 defmodule Main do
   def perform do
     user = %User{name: "motty", age: 31}
+    IO.inspect(inspect(user), label: "kernel inspect")
     IO.inspect(user.name, label: "name")
     IO.inspect(user.age, label: "age")
   end
