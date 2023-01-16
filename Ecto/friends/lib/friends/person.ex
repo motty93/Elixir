0be05ab15ae628d7e92d8355974c85d6bf55bf2b
motty93 <rdwbocungelt5@gmail.com>
2023-01-16 21:16:15 +0900

    refactor: formatter

--- Ecto/friends/lib/friends/person.ex
+++ Ecto/friends/lib/friends/person.ex
@@ -5,14 +5,15 @@ defmodule Friends.Person do
   import Ecto.Changeset
 
   schema "people" do
-    field :name, :string
-    field :age, :integer, default: 0
+    field(:name, :string)
+    field(:age, :integer, default: 0)
   end
 
   # changesetでvalidation追加
   def changeset(struct, params) do
     struct
-    |> cast(params, [:name, :age]) # changeset objectになる
+    # changeset objectになる
+    |> cast(params, [:name, :age])
     |> set_name_if_anonymous()
     |> validate_fictional_name()
   end
@@ -26,7 +27,7 @@ defmodule Friends.Person do
   # カスタムバリデーション
   # instance variable none equal
   @fictional_names ["Black Panther", "Wonder Woman", "Spiderman"]
-  defp validate_fictional_name(changeset) do
+  def validate_fictional_name(changeset) do
     name = get_field(changeset, :name)
 
     if name in @fictional_names do
@@ -36,7 +37,7 @@ defmodule Friends.Person do
     end
   end
 
-  defp set_name_if_anonymous(changeset) do
+  def set_name_if_anonymous(changeset) do
     name = get_field(changeset, :name)
 
     if is_nil(name) do
