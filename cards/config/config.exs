5151b1e45e532493ae43dec259b6a451e971b16c
motty93 <rdwbocungelt5@gmail.com>
2023-02-09 22:38:07 +0900

    feat: section2

---
+++ cards/config/config.exs
@@ -0,0 +1,7 @@
+import Config
+if Mix.env() == :dev do
+  config :mix_test_watch,
+    tasks: [
+      "credo"
+    ]
+end
--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,70 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "hi there!"
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size) # 第一引数に入る
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # cards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+    cards =
+      for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+      end
+
+    cards |> List.flatten()
+  end
+
+  def shuffle(deck) do
+    deck |> Enum.shuffle()
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    # tuple
+    # 取得する際はパターンマッチを使用する
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    # {status, binary} = File.read(filename)
+    case File.read(filename) do
+      {:ok, binary} ->
+        IO.puts(:ok)
+        :erlang.binary_to_term(binary)
+
+      {:error, _message} ->
+        "That file does not exist."
+    end
+  end
+
+  def test("hoge") do
+    "hoge" |> IO.puts()
+  end
+
+  def test("fuga") do
+    "fuga" |> IO.puts()
   end
 end
