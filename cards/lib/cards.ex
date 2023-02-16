2bfa939f39d0963af75ac11a932b83f2a78d2fea
motty93 <rdwbocungelt5@gmail.com>
2023-02-16 20:24:27 +0900

    feat: document & mix test

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -13,6 +13,9 @@ defmodule Cards do
     |> Cards.deal(hand_size) # 第一引数に入る
   end
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -35,10 +38,35 @@ defmodule Cards do
     deck |> Enum.shuffle()
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+       "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+       "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+       "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+       "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards shou8ld be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     # tuple
     # 取得する際はパターンマッチを使用する
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,8 +1,20 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
+  doctest Cards # doc内のコードをテスト実行する
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert Cards.hello() == "hi there!"
+  end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    # assert deck != Cards.shuffle(deck)
+    # 否定のテスト
+    refute deck == Cards.shuffle(deck)
   end
 end
