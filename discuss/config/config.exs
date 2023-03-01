2eb3cebb05c3f838d3f0a7bb63c3a24b73564eea
motty93 <rdwbocungelt5@gmail.com>
2023-03-01 23:15:13 +0900

    chore: credo

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -50,3 +50,11 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+
+if Mix.env() == :dev do
+  config :mix_test_watch,
+    tasks: [
+      "credo"
+    ]
+end
--- discuss/mix.exs
+++ discuss/mix.exs
@@ -47,7 +47,9 @@ defmodule Discuss.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
