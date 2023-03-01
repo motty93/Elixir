f3695cec60f0a8ac4a1d789ed4fbf222598f1ee2
motty93 <rdwbocungelt5@gmail.com>
2023-03-01 21:19:39 +0900

    feat: mirror_row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -58,7 +58,7 @@ defmodule Identicon do
     grid =
       hex
       |> Enum.chunk(3)
-      |> Enum.map(&mirror_row(&1))
+      |> Enum.map(&mirror_row/1)
       |> List.flatten()
       |> Enum.with_index()
 
