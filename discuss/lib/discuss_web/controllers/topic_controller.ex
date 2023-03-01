8030dc72f9199cbf129c8cf865c3980b1f5f6970
motty93 <rdwbocungelt5@gmail.com>
2023-03-01 23:15:53 +0900

    feat: topic controller

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,13 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  def new(conn, params) do
+    IO.puts("++++++++")
+    IO.inspect(conn)
+    IO.puts("++++++++")
+    IO.inspect(conn.resp_headers)
+    IO.puts("++++++++")
+    IO.inspect(params)
+    IO.puts("++++++++")
+  end
+end
