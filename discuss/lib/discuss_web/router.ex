0d17570567d5af0fc8cc608587d81bbe0172c852
motty93 <rdwbocungelt5@gmail.com>
2023-03-01 23:15:43 +0900

    feat: topics routing

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -18,20 +18,10 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    get "/topics/new", TopicController, :new
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", DiscussWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
